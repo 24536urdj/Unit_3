d7019e657b6c387bd5761e1647cc704e7d22ce0d
24536urdj <112072887+24536urdj@users.noreply.github.com>
2023-03-16 20:30:11 +0900

    Update and rename quiz_43.md to quiz_43.py

--- quiz/quiz_43.py
+++ quiz/quiz_43.py
@@ -1,5 +1,5 @@
-'''.py 
-  CREATE TABLE if not exists movies(id integer primary key,name TEXT,producer TEXT,year INT,director TEXT,category TEXT,budget TEXT)
+''' 
+CREATE TABLE if not exists movies(id integer primary key,name TEXT,producer TEXT,year INT,director TEXT,category TEXT,budget TEXT)
 Insert into movies values(1,"harry potter","David Heyman",2001,"Chris Columbus","fiction","$1.2 billion");
 Insert into movies values(2,"The conjuring","Tony DeRosa-Grund",2013,"James Wan","Horror","$20 million")
 '''
